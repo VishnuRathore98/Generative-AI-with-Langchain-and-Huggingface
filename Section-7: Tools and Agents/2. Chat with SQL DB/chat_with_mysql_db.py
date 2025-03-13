effcab6c03a07c1bd02eb6dab02d0d038e837fdc
VishnuRathore98 <rathorevishnu666@gmail.com>
2025-03-13 07:38:46 +0530

    Add: SQLToolkit and create SQL agent.

--- Section-7: Tools and Agents/2. Chat with SQL DB/chat_with_mysql_db.py
+++ Section-7: Tools and Agents/2. Chat with SQL DB/chat_with_mysql_db.py
@@ -1,6 +1,10 @@
 import streamlit as st
 from pathlib import Path
+from langchain.agents import create_sql_agent
 from langchain.sql_database import SQLDatabase
+from langchain.agents.agent_types import AgentType
+from langchain.callbacks import StreamlitCallbackHandler
+from langchain.agents.agent_toolkits import SQLDatabaseToolkit
 from sqlalchemy import create_engine
 import sqlite3
 from langchain_groq import ChatGroq
@@ -61,3 +65,52 @@ else:
     db = configure_db(db_uri)
 
 # toolkit
+toolkit = SQLDatabaseToolkit(db=db, llm=llm)
+
+agent = create_sql_agent(
+    llm=llm,
+    toolkit=toolkit,
+    verbose=True,
+    agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    handle_parsing_errors=True
+)
+
+if "messages" not in st.session_state or st.sidebar.button("Clear message history"):
+    st.session_state["messages"] = [
+        {"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+user_query = st.chat_input(placeholder="Ask anything from the database")
+
+if user_query:
+    st.session_state.messages.append({"role": "user", "content": user_query})
+    st.chat_message("user").write(user_query)
+
+    with st.chat_message("assistant"):
+        streamlit_callback = StreamlitCallbackHandler(st.container())
+        response = agent.run(user_query, callbacks=[streamlit_callback])
+        response = agent.run(user_query)
+        st.session_state.messages.append(
+            {"role": "assistant", "content": response})
+        st.write(response)
+
+
+# Conceptual flow:
+
+# 1️⃣ User Interaction
+# The user selects a database.
+# Enters a natural language query.
+
+# 2️⃣ LLM Processing
+# The AI understands the query.
+# Converts it into an SQL query.
+
+# 3️⃣ Query Execution
+# The AI executes the query on the database.
+# Retrieves relevant results.
+
+# 4️⃣ Response Display
+# The response is formatted and displayed in the chat.
+# Stored in session history for continuity.
